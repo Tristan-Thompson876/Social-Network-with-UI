6ae9eb2319a1417f9d6f0bd3cc935659ed257c37
Tristan-Thompson876 <125337721+Tristan-Thompson876@users.noreply.github.com>
2023-11-27 23:49:26 -0500

    aa

--- p2/ExternalLinkPost.java
+++ p2/ExternalLinkPost.java
@@ -6,106 +6,81 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 import java.net.URL;
-
 import javax.swing.JEditorPane;
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
-
 import p1.enums.PostAudience;
 
 public class ExternalLinkPost extends Post {
-	// immutable instance data
-	private Content url;
+    private Content url; // immutable instance data
 
-	public ExternalLinkPost(PostAudience sharedWith, String url) {
-		// assumes url is valid, i.e., getUrlHTMLContents and display won't throw a
-		// MalformedURLException nor an IOException respectively
-		super(sharedWith);
-		this.url = new Content(url);
-	}
-
-	@Override
-	public String toString() {
-		return "TBD";
-	}
-
-	// can be used to test if a URL format is valid
-	// from https://www.baeldung.com/java-validate-url
-	public boolean urlIsValid() {
-		try {
-			new URL(url.toString()).toURI();
-			return true;
-		} catch (MalformedURLException | URISyntaxException e) {
-			return false;
-		}
-	}
-
-	// can be used to test if a URL exists
-	// from https://www.baeldung.com/java-check-url-exists
-	public boolean urlExists() {
-		try {
-			URL urlchk = new URL(url.toString());
-			HttpURLConnection huc = (HttpURLConnection) urlchk.openConnection();
-			return huc.getResponseCode() == HttpURLConnection.HTTP_OK;
-		} catch (IOException e) {
-			return false;
-		}
-	}
-
-	// from
-	// https://stackoverflow.com/questions/54054653/how-do-i-draw-a-full-webpage-using-java-swing
-	@Override
-	public void display() {
-		JEditorPane jep = new JEditorPane();
-		jep.setEditable(false);
-
-		try {
-			jep.setPage(url.toString());
-		} catch (IOException e) {
-			jep.setContentType("text/html");
-			jep.setText("<html>Could not load</html>");
-		}
-
-		JScrollPane scrollPane = new JScrollPane(jep);
-		JFrame f = new JFrame("Post: " + getID());
-		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		f.getContentPane().add(scrollPane);
-		f.setPreferredSize(new Dimension(500, 300));
-		f.pack();
-		f.setVisible(true);
-	}
-
-	// testing the URL methods
-	public static void main(String[] args) {
-		ExternalLinkPost elp;
-
-		elp = new ExternalLinkPost(PostAudience.Public, "://www.jovatpoint.com/URL-class");
-		if (elp.urlIsValid())
-			if (elp.urlExists())
-				elp.display();
-			else
-				System.out.println("URL does not exist");
-		else
-			System.out.println("URL invalid");
-
-		elp = new ExternalLinkPost(PostAudience.Public, "https://www.jovatpoint.com/URL-class");
-		if (elp.urlIsValid())
-			if (elp.urlExists())
-				elp.display();
-			else
-				System.out.println("URL does not exist");
-		else
-			System.out.println("URL invalid");
-
-		elp = new ExternalLinkPost(PostAudience.Public, "https://www.javatpoint.com/URL-class");
-
-		if (elp.urlIsValid() && elp.urlExists())
-			elp.display();
-
-	}
-
-    public String getUrl() {
-        return null;
+    public ExternalLinkPost(PostAudience sharedWith, String url) {
+        super(sharedWith);
+        this.url = new Content(url);
     }
 
+    @Override
+    public String toString() {
+        return "ExternalLinkPost{ID=" + getID() + ", URL=" + url + "}";
+    }
+
+    public boolean urlIsValid() {
+        try {
+            new URL(url.toString()).toURI();
+            return true;
+        } catch (MalformedURLException | URISyntaxException e) {
+            return false;
+        }
+    }
+
+    public boolean urlExists() {
+        try {
+            URL urlchk = new URL(url.toString());
+            HttpURLConnection huc = (HttpURLConnection) urlchk.openConnection();
+            return huc.getResponseCode() == HttpURLConnection.HTTP_OK;
+        } catch (IOException e) {
+            return false;
+        }
+    }
+
+    @Override
+    public void display() {
+        JEditorPane jep = new JEditorPane();
+        jep.setEditable(false);
+
+        try {
+            jep.setPage(url.toString());
+        } catch (IOException e) {
+            jep.setContentType("text/html");
+            jep.setText("<html>Could not load</html>");
+        }
+
+        JFrame f = createDisplayFrame(jep);
+        f.setVisible(true);
+    }
+
+    private JFrame createDisplayFrame(JEditorPane jep) {
+        JScrollPane scrollPane = new JScrollPane(jep);
+        JFrame f = new JFrame("Post: " + getID());
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.getContentPane().add(scrollPane);
+        f.setPreferredSize(new Dimension(500, 300));
+        f.pack();
+        return f;
+    }
+
+    public static void main(String[] args) {
+        testURL("://www.jovatpoint.com/URL-class");
+        testURL("https://www.jovatpoint.com/URL-class");
+        testURL("https://www.javatpoint.com/URL-class");
+    }
+
+    private static void testURL(String url) {
+        ExternalLinkPost elp = new ExternalLinkPost(PostAudience.Public, url);
+        if (elp.urlIsValid() && elp.urlExists()) {
+            elp.display();
+        } else {
+            System.out.println("URL invalid or does not exist: " + url);
+        }
+    }
 }
